ba07dee21946b80af23a01381655c3d0fcb7d5d1
Lyw0 <2687391644@qq.com>
2021-11-16 10:06:13 +0800

    https://github.com/Lyw0/shizuku.git

--- app/src/main/java/com/z209200731/beatbox/SoundViewModel.kt
+++ app/src/main/java/com/z209200731/beatbox/SoundViewModel.kt
@@ -1,16 +1,19 @@
 package com.z209200731.beatbox
 
 import androidx.databinding.BaseObservable
-import androidx.databinding.Bindable
 
+class SoundViewModel(private val beatBox: BeatBox) : BaseObservable() {
+    fun onButtonClicked() {
+        sound?.let {
+            beatBox.play(it)
+        }
+    }
 
-class SoundViewModel : BaseObservable() {
     var sound: Sound? = null
         set(sound) {
             field = sound
             notifyChange()
         }
-
 
     val title: String?
         get() = sound?.name
--- app/src/test/java/com/z209200731/beatbox/ExampleUnitTest.kt
+++ app/src/test/java/com/z209200731/beatbox/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.z209200731.beatbox
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
